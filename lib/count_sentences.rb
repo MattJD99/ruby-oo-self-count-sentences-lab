803e631150b2c10793bd9afa352b33b2c7bb64b9
Matt D <dayclawtel@gmail.com>
2021-03-04 11:33:50 -0800

    commit tree ae49a451ee7e3bfcec7d259c5342f75a7b7c33c3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    #sentences = []
+    #self.split(" ").each do |character|
+      #if character == "?" || character == "." || character == "!"
+        #sentences.push charachter
+      #end
+    #end
+    #sentences.length
+    self.split(/[?!.]+/).count
   end
 end
